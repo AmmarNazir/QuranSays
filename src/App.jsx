489fc7472f5805ebca0194fc67c42e4ffaa47646
Muhammad Ammar Nazir <96984573+AmmarNazir@users.noreply.github.com>
2025-09-13 15:58:24 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl text-green-600 font-extrabold">
+        ✅ Quran Says Starter is Working
+      </h1>
+    </div>
+  );
+}
